ec8c87ec8d01883370a11dd0b61af3e89109cc03
Tasneem Karwa <tasneemkarwa@gmail.com>
2019-02-23 19:06:55 +0000

    components completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'review/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "cocktails#index"
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:create, :new]
+    resources :review, only: [:create, :new]
   end
   resources :doses, only: [:destroy]
 end
